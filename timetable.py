85eb4ebf2764c7c383e8d9850eb20a219d40a33b
gururajbn <n.gururajbn@gmail.com>
2014-02-20 21:43:13 +0530

    documented the code

--- timetable.py
+++ timetable.py
@@ -19,6 +19,7 @@ def displyTimeTable(indicator,day):
 
 week=['sun','mon','tue','wed','thu','fri','sat']
 
+#This dict represents the time slots of lecture of a normal college day
 time={	0:'08:00 AM',
 		1:'09:00 AM',
 		2:'10:30 AM',
@@ -28,16 +29,19 @@ time={	0:'08:00 AM',
 		6:'03:30 PM',
 	}
 
-subjects={	0:"        FREE           ",
+# Change the subject name according to your electives
+subjects={	
+			0:"        FREE           ",
 			1:"    Comp Graphics      ",
 			2:"    Comp Networks      ",
 			3:"Object  Design Modeling",
 			4:"Artificial Intelligence",
 			5:"Advance Data Structures",
-			6:"Mini Project assessment",
 
 		}
 
+#Change the class timings by altering the subject code from Subject and Time lists
+# ex : 'sat':[0,4] represents , subject[0] at time[0] & subject[4] at time[1]
 time_table= {	
 				'mon':[3,3,0,2,4],
 				'tue':[0,0,0,1,2,5,5],
